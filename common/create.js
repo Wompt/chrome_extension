178ca22b32ddeb5f4d11cf4e7836952c9d84dc5d
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-06-07 01:18:50 -0700

    Change embed.js to create.js

--- common/create.js
+++ common/create.js
@@ -1,4 +1,4 @@
-wompt.embed = {
+wompt.create = {
 	iframe: function(room_name, text_color){
 		text_color = text_color || '444';
 		
@@ -8,5 +8,12 @@ wompt.embed = {
 			,width: '95%'
 			,style: 'border:none; display:block; margin:0 auto;'
 		})
+	},
+	
+	link: function(room_name, text, classes){
+		return wompt.util.createElement('a',{
+			href: wompt.settings.urlPrefix +	room_name,
+			'class': classes
+		}, text || room_name)
 	}
 }
