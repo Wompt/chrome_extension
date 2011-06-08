bd5fcbedd9e46ea870cd92a638c10c53f6bde31a
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-06-07 23:22:15 -0700

    Store expanded and collapsed state in localStorage

--- github/links_and_iframes.js
+++ github/links_and_iframes.js
@@ -1,25 +1,30 @@
 function addIFrame(){
 	var repo_head = document.getElementsByClassName('repohead')[0];
+	// make sure we only add the iframe on the main project page
+	// on all other pages, the repo_head div has a 'shortdetails' class
 	if(repo_head && repo_head.className.indexOf('shortdetails') < 0){
+		
 		var container = wompt.util.createElement('div', {style:'margin:.1em 0 .6em 0;'});
 		var iframe = wompt.create.iframe('github' + window.location.pathname);
+		
 		wompt.util.applyAttributes(iframe, {
 			width: '100%',
-			height: expandedState() ? '300px' : '0',
+			height: collapsedState() ? '0' : '300px',
 			style: 'display:block; border:none; -webkit-transition:height .7s ease-in-out;'
 		});
-		container.appendChild(createToggleLink(iframe, expandedState()));
+		
+		container.appendChild(createToggleLink(iframe, collapsedState()));
 		container.appendChild(iframe);
 		repo_head.parentNode.insertBefore(container, repo_head.nextSibling)
 	}
 }
 
-function expandedState(state){
-	if(state == null) return localStorage['github_expanded'] == 'true'
-	return localStorage['github_expanded'] = state;
+function collapsedState(state){
+	if(state == null) return localStorage['github_collapsed'] == 'true'
+	return localStorage['github_collapsed'] = state;
 }
 
-function createToggleLink(iframe_to_toggle, expanded){
+function createToggleLink(iframe_to_toggle, collapsed){
 	var a = wompt.util.createElement('a', {
 		'href':'#',
 		'class': 'breadcrumb',
@@ -27,15 +32,15 @@ function createToggleLink(iframe_to_toggle, expanded){
 	}, toggleText());
 	
 	function toggleText(){
-		return expanded ? "close chat -" : "expand chat +";
+		return collapsed ? "expand chat +" : "close chat -";
 	}
 	
 	a.addEventListener('click', function(e){
-		expanded = !expanded;
-		iframe_to_toggle.setAttribute('height', expanded ? '300px' : '0');
+		collapsed = !collapsed;
+		iframe_to_toggle.setAttribute('height', collapsed ? '0' : '300px');
 		this.innerText = toggleText();
 		e.preventDefault();
-		expandedState(expanded);
+		collapsedState(collapsed);
 	});
 	return a;
 }
